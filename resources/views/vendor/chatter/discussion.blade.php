d0e221645adaee3968c7dc0b4e9b56d8fdf8b095
atlas <rdennis10@hawkmail.hccfl.edu>
2018-07-07 19:11:09 -0400

    minor revert, fixed up threads really nicely, edit soonTM

--- resources/views/vendor/chatter/discussion.blade.php
+++ resources/views/vendor/chatter/discussion.blade.php
@@ -23,23 +23,12 @@
         $db_field = Config::get('chatter.user.avatar_image_database_field');
     @endphp
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="p-4 bg-brand-darkest-grey">
             <div class="d-flex flex-row">
-                <a class="my-auto pr-5" href="/{{ Config::get('chatter.routes.home') }}"><i class="fas fa-chevron-left"></i></a>
-                <h1 class="my-auto text-white pr-3">{{ $discussion->title }}</h1>
-                <div class="flex-column w-75 my-auto">
-                    <div class="text-white-50">
-                        @lang('chatter::messages.discussion.head_details')
-                    </div>
-                    <a href="/{{ Config::get('chatter.routes.home') }}/{{ Config::get('chatter.routes.category') }}/{{ $discussion->category->slug }}">
-                        {{ $discussion->category->name }}</a>
-                    <div class="text-white-50">
-                        by: {{$author[0]->name}}
-                    </div>
-                </div>
-                <div class="flex-row mx-auto my-auto">
-                    <div class="avatar">
+                <a class="my-auto px-2" href="/{{ Config::get('chatter.routes.home') }}"><i class="fas fa-chevron-left"></i></a>
+                <div class="my-auto px-2">
+                    <div class="avatar" style="width: 5rem; height: auto;">
                     @if(Config::get('chatter.user.avatar_image_database_field'))
                         <!-- If the user db field contains http:// or https:// we don't need to use the relative path to the image assets -->
                             @if( (substr($author[0]->{$db_field}, 0, 7) == 'http://') || (substr($author[0]->{$db_field}, 0, 8) == 'https://') )
@@ -55,6 +44,13 @@
                         @endif
                     </div>
                 </div>
+                <div class="flex-column flex-fill my-auto p-2">
+                    <h3 class="my-auto text-white">{{ $discussion->title }}</h3>
+                   <div class="d-flex flex-column">
+                       <a  class="pr-1" href="/{{ Config::get('chatter.routes.home') }}/{{ Config::get('chatter.routes.category') }}/{{ $discussion->category->slug }}">Category: {{ $discussion->category->name }}</a>
+                       <div class="text-white-50">by: {{$author[0]->name}}</div>
+                   </div>
+                </div>
                 @if(!Auth::guest() && (Auth::user()->id == $discussion->user_id))
                     <div class="flex-row mx-auto my-auto px-1">
                         <div class="btn btn-primary text-white-50 edit-post">
@@ -67,7 +63,6 @@
                         <button type="submit" class="btn btn-danger">Delete</button>
                     </form>
                 @endif
-
             </div>
         </div>
 
@@ -97,13 +92,14 @@
                 </div>
             @endif
         @endif
+
         <div class="d-flex flex-column flex-fill mt-5">
             @foreach($posts as $post)
-           @if($loop->first)
-                    <div class="p-3 bg-brand-darkest-grey mb-5" style="display:block;">
+                @if($loop->first)
+                    <div class="bg-brand-darkest-grey mb-5 rounded">
                         <div class="d-flex flex-row">
-                            <div class="flex-column mx-auto my-auto">
-                                <div class="avatar">
+                            <div class="flex-column mx-auto p-3 author-overlay">
+                                <div class="avatar" style="width: 10rem; height: 10rem;">
                                     @if(Config::get('chatter.user.avatar_image_database_field'))
                                         @if( (substr($post->user->{$db_field}, 0, 7) == 'http://') || (substr($post->user->{$db_field}, 0, 8) == 'https://') )
                                             <img src="{{ $post->user->{$db_field}  }}">
@@ -117,80 +113,72 @@
 					        				</span>
                                     @endif
                                 </div>
-                                <div class="mx-auto my-auto text-center">
-                                    <a href="{{ \App\Helpers\ChatterHelper::userLink($post->user) }}">{{ ucfirst($post->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</a>
-                                </div>
-
-                                @if(!Auth::guest() && (Auth::user()->id == $post->user->id))
-                                @endif
-                            </div>
-                            <div class="flex-row flex-fill flex- my-auto p-5">
-                                <div class="text-white-50">
-                                    <?= $post->body; ?>
+                                <div class="mx-auto my-auto pt-2 text-center">
+                                    <a class="h3 text-light" href="{{ \App\Helpers\ChatterHelper::userLink($post->user) }}">{{ ucfirst($post->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</a>
                                 </div>
                             </div>
-                            <div class="d-flex flex-row">
-                                <div class="px-1 mx-auto my-auto">
-                                    <div class="btn btn-primary text-white-50 edit-post">
-                                        <i class="chatter-edit"></i> @lang('chatter::messages.words.edit')
+                            <div class="d-flex flex-column flex-fill align-self-stretch">
+                                <div class="text-muted pl-4 pt-2">Posted: {{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->created_at))->diffForHumans() }}</div>
+                                <div class="flex-row flex-fill align-self-stretch p-4 border-bottom border-brand-dark-grey text-white-50">
+                                    {!! $post->body !!}
+                                </div>
+                                <div class="d-flex flex-row p-2 align-items-end ml-auto">
+                                    <div class="px-1">
+                                        <div class="btn btn-primary text-white-50 edit-post">
+                                            <i class="chatter-edit"></i> @lang('chatter::messages.words.edit')
+                                        </div>
                                     </div>
+                                    <form class="px-1 m-0" action="{{ action('ChatterPostController@destroy',$post->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
                                 </div>
-                                <form class="px-1 mx-auto my-auto" action="{{ action('ChatterPostController@destroy',$post->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                                <div class="flex-column mt-auto text-white-50 px-3">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->created_at))->diffForHumans() }}</div>
-
                             </div>
                         </div>
                     </div>
                 @else
-               <div class="p-3 bg-brand-darkest-grey" style="display:block;">
-                   <div class="d-flex flex-row">
-                       <div class="flex-column mx-auto my-auto">
-                           <div class="avatar">
-                               @if(Config::get('chatter.user.avatar_image_database_field'))
-                                   @if( (substr($post->user->{$db_field}, 0, 7) == 'http://') || (substr($post->user->{$db_field}, 0, 8) == 'https://') )
-                                       <img src="{{ $post->user->{$db_field}  }}">
-                                   @else
-                                       <img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . $post->user->{$db_field}  }}">
-                                   @endif
+                    <div class="bg-brand-darkest-grey mb-5 rounded">
+                        <div class="d-flex flex-row">
+                            <div class="flex-column mx-auto p-3 author-overlay">
+                                <div class="avatar" style="width: 10rem; height: 10rem;">
+                                    @if(Config::get('chatter.user.avatar_image_database_field'))
+                                        @if( (substr($post->user->{$db_field}, 0, 7) == 'http://') || (substr($post->user->{$db_field}, 0, 8) == 'https://') )
+                                            <img src="{{ $post->user->{$db_field}  }}">
+                                        @else
+                                            <img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . $post->user->{$db_field}  }}">
+                                        @endif
 
-                               @else
-                                   <span class="chatter_avatar_circle" style="background-color:#<?= \App\Helpers\ChatterHelper::stringToColorCode($post->user->{Config::get('chatter.user.database_field_with_user_name')}) ?>">
+                                    @else
+                                        <span class="chatter_avatar_circle" style="background-color:#<?= \App\Helpers\ChatterHelper::stringToColorCode($post->user->{Config::get('chatter.user.database_field_with_user_name')}) ?>">
 					        					{{ ucfirst(substr($post->user->{Config::get('chatter.user.database_field_with_user_name')}, 0, 1)) }}
 					        				</span>
-                               @endif
-                           </div>
-                           <div class="mx-auto my-auto text-center">
-                               <a href="{{ \App\Helpers\ChatterHelper::userLink($post->user) }}">{{ ucfirst($post->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</a>
-                           </div>
-
-                           @if(!Auth::guest() && (Auth::user()->id == $post->user->id))
-                           @endif
-                       </div>
-                       <div class="flex-row flex-fill flex- my-auto p-5">
-                           <div class="text-white-50">
-                               <?= $post->body; ?>
-                           </div>
-                       </div>
-                       <div class="d-flex flex-row">
-                           <div class="px-1 mx-auto my-auto">
-                               <div class="btn btn-primary text-white-50 edit-post">
-                                   <i class="chatter-edit"></i> @lang('chatter::messages.words.edit')
-                               </div>
-                           </div>
-                           <form class="px-1 mx-auto my-auto" action="{{ action('ChatterPostController@destroy',$post->id) }}" method="POST">
-                               @csrf
-                               @method('DELETE')
-                               <button type="submit" class="btn btn-danger">Delete</button>
-                           </form>
-                           <div class="flex-column mt-auto text-white-50 px-3">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->created_at))->diffForHumans() }}</div>
-
-                       </div>
-                   </div>
-               </div>
+                                    @endif
+                                </div>
+                                <div class="mx-auto my-auto pt-2 text-center">
+                                    <a class="h3 text-light" href="{{ \App\Helpers\ChatterHelper::userLink($post->user) }}">{{ ucfirst($post->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</a>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-column flex-fill align-self-stretch">
+                                <div class="text-muted pl-4 pt-2">Posted: {{ \Carbon\Carbon::createFromTimeStamp(strtotime($post->created_at))->diffForHumans() }}</div>
+                                <div class="flex-row flex-fill align-self-stretch p-4 border-bottom border-brand-dark-grey text-white-50">
+                                    {!! $post->body !!}
+                                </div>
+                                <div class="d-flex flex-row p-2 align-items-end ml-auto">
+                                    <div class="px-1">
+                                        <div class="btn btn-primary text-white-50 edit-post">
+                                            <i class="chatter-edit"></i> @lang('chatter::messages.words.edit')
+                                        </div>
+                                    </div>
+                                    <form class="px-1 m-0" action="{{ action('ChatterPostController@destroy',$post->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endif
             @endforeach
         </div>
@@ -199,39 +187,39 @@
     <div id="pagination">{{ $posts->links() }}</div>
 
     @if(!Auth::guest())
-        <div id="new_response">
-            <div class="chatter_avatar">
-            @if(Config::get('chatter.user.avatar_image_database_field'))
-                <?php $db_field = Config::get('chatter.user.avatar_image_database_field'); ?>
-                <!-- If the user db field contains http:// or https:// we don't need to use the relative path to the image assets -->
-                    @if( (substr(Auth::user()->{$db_field}, 0, 7) == 'http://') || (substr(Auth::user()->{$db_field}, 0, 8) == 'https://') )
-                        <img src="{{ Auth::user()->{$db_field}  }}">
-                    @else
-                        <img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . Auth::user()->{$db_field}  }}">
-                    @endif
+            <div class="container-fluid" id="editor">
+<div class="d-flex mx-auto flex-row align-items-center align-items-stretch">
+    <div class="chatter_loader dark" id="new_discussion_loader">
+        <div></div>
+    </div>
+    <form class="d-flex mx-auto flex-column" id="chatter_form_editor" action="/{{ Config::get('chatter.routes.home') }}/posts" method="POST">
+        <div class="avatar mr-2">
+        @if(Config::get('chatter.user.avatar_image_database_field'))
+            <?php $db_field = Config::get('chatter.user.avatar_image_database_field'); ?>
+            <!-- If the user db field contains http:// or https:// we don't need to use the relative path to the image assets -->
+                @if( (substr(Auth::user()->{$db_field}, 0, 7) == 'http://') || (substr(Auth::user()->{$db_field}, 0, 8) == 'https://') )
+                    <img src="{{ Auth::user()->{$db_field}  }}">
                 @else
-                    <span class="chatter_avatar_circle" style="background-color:#<?= \App\Helpers\ChatterHelper::stringToColorCode(Auth::user()->{Config::get('chatter.user.database_field_with_user_name')}) ?>">
+                    <img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . Auth::user()->{$db_field}  }}">
+                @endif
+            @else
+                <span class="avatar" style="background-color:#<?= \App\Helpers\ChatterHelper::stringToColorCode(Auth::user()->{Config::get('chatter.user.database_field_with_user_name')}) ?>">
 		        					{{ strtoupper(substr(Auth::user()->{Config::get('chatter.user.database_field_with_user_name')}, 0, 1)) }}
 		        				</span>
-                @endif
-            </div>
-
-            <div id="new_discussion">
-                <div class="chatter_loader dark" id="new_discussion_loader">
-                    <div></div>
-                </div>
-                <form id="chatter_form_editor" action="/{{ Config::get('chatter.routes.home') }}/posts" method="POST">
-                    <input type="hidden" name="_token" id="csrf_token_field" value="{{ csrf_token() }}">
-                    <input type="hidden" name="chatter_discussion_id" value="{{ $discussion->id }}">
-                    <!-- BODY -->
-                    <div id="editor">
-                        <textarea class="trumbowyg" name="body" placeholder="Type Your Discussion Here...">{{ old('body') }}</textarea>
-                    </div>
-                    <button id="submit_response" class="btn btn-success pull-right"><i class="chatter-new"></i> @lang('chatter::messages.response.submit')</button>
-                </form>
-
-            </div><!-- #new_discussion -->
+            @endif
         </div>
+        <input type="hidden" name="_token" id="csrf_token_field" value="{{ csrf_token() }}">
+        <input type="hidden" name="chatter_discussion_id" value="{{ $discussion->id }}">
+        <!-- BODY -->
+        <div id="editor">
+            <textarea class="trumbowyg" name="body" placeholder="Type Your Discussion Here...">{{ old('body') }}</textarea>
+        </div>
+        <button id="submit_response" class="btn btn-success pull-right"><i class="chatter-new"></i> @lang('chatter::messages.response.submit')</button>
+    </form>
+
+</div>
+            </div><!-- #new_discussion -->
+
     @else
         <div id="login_or_register">
             <p>@lang('chatter::messages.auth', ['home' => Config::get('chatter.routes.home')])</p>
