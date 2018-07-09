aeb290799a19b17d77def361d1dfe8edc85b10fd
atlas <rdennis10@hawkmail.hccfl.edu>
2018-07-09 01:52:55 -0400

    forgot to turn off staff page

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -11,13 +11,14 @@ class StaffController extends Controller
 {
     public function index(){
 
-        $staff = new Staff();
-        $staff = DB::connection('mysql5')
-            ->table('serverguard_users')
-            ->whereIn('rank', ['founder', 'developer', 'community_manager', 'superadmin', 'senior_administrator', 'admin', 'senior_moderator', 'moderator', 'trial_moderator'])
-            ->orderByRaw('(CASE rank WHEN \'founder\' THEN 1 WHEN \'developer\' THEN 2 WHEN \'community_manager\' THEN 3 WHEN \'superadmin\' THEN 4 WHEN \'senior_administrator\' THEN 5 WHEN \'admin\' THEN 6 WHEN \'senior_moderator\' THEN 7 WHEN \'moderator\' THEN 8 WHEN \'trial_moderator\' THEN 9 ELSE 100 END) ASC, rank ASC')
-            ->simplePaginate(15);
-        return view('front.staff')->with(['staff' => $staff]);
+//        $staff = new Staff();
+//        $staff = DB::connection('mysql5')
+//            ->table('serverguard_users')
+//            ->whereIn('rank', ['founder', 'developer', 'community_manager', 'superadmin', 'senior_administrator', 'admin', 'senior_moderator', 'moderator', 'trial_moderator'])
+//            ->orderByRaw('(CASE rank WHEN \'founder\' THEN 1 WHEN \'developer\' THEN 2 WHEN \'community_manager\' THEN 3 WHEN \'superadmin\' THEN 4 WHEN \'senior_administrator\' THEN 5 WHEN \'admin\' THEN 6 WHEN \'senior_moderator\' THEN 7 WHEN \'moderator\' THEN 8 WHEN \'trial_moderator\' THEN 9 ELSE 100 END) ASC, rank ASC')
+//            ->simplePaginate(15);
+//        return view('front.staff')->with(['staff' => $staff]);
+        return view('errors.503');
     }
 }
 
